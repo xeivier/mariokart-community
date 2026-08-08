c5592c36234d1b880494d1ca58a37121f3802638
Javi <javi@Javis-MacBook-Air.local>
2026-08-08 12:36:50 +0200

    Initial commit. Bootstrap project

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  const port = config.get<number>('PORT', 3000);
+
+  await app.listen(port);
+
+  console.log(`🚀 Server running on http://localhost:${port}`);
+}
+
+bootstrap();
